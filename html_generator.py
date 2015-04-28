69a26af9315384cb590bb906e1a825d21c26895d
Liz1325 <el1643@att.com>
2015-04-28 18:57:25 -0500

    Committed by Udacity

---
+++ html_generator.py
@@ -0,0 +1,27 @@
+html_file=open ('index.html', 'w')
+
+def generate_concept_HTML(concept_title, concept_description):
+    html_text_1 = '''
+<div class="concept">
+    <div class="concept-title">
+        ''' + concept_title
+    html_text_2 = '''
+    </div>
+    <div class="concept-description">
+        ''' + concept_description
+    html_text_3 = '''
+    </div>
+</div>'''
+    full_html_text = html_text_1 + html_text_2 + html_text_3
+    return full_html_text
+
+def get_title(concept):
+    start_location = concept.find('TITLE:')
+    end_location = concept.find('DESCRIPTION:')
+    title = concept[start_location+7 : end_location-1]
+    return title
+
+def get_description(concept):
+    start_location = concept.find('DESCRIPTION:')
+    description = concept[start_location+13 :]
+    return description
